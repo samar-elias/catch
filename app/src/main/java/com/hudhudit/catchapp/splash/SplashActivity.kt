0e8efabcf63b53d2a82972bd8f37f669c2377ad7
samar-elias <76810297+samar-elias@users.noreply.github.com>
2022-12-01 10:59:42 +0300

    set up

---
+++ app/src/main/java/com/hudhudit/catchapp/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.hudhudit.catchapp.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hudhudit.catchapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
